0115072cecd144d01d83b06fb7e8ba90786527b8
codebynishan <mail.nishanrana@gmail.com>
2025-12-17 12:33:52 +0545

    feat:DataBase Connection

--- db.py
+++ db.py
@@ -1,16 +1,19 @@
 import psycopg2
 import pandas as pd
 from psycopg2 import OperationalError, Error
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def get_connection():
     try:
         conn = psycopg2.connect(
-            host="localhost",
-            database="routinedb",
-            user="postgres",
-            password="Nishan@2059",
-            port="5432"
+             host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
         )
         return conn
 
